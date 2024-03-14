4f68f2e11b2fb46273d6efaf296d3e5bcb9fa220
Deyvid Rannyere <deyran@gmail.com>
2024-03-14 11:04:29 -0300

    updates

---
+++ chapter-03/Quick/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+    }
+}
